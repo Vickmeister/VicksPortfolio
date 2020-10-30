df93c528a4b891d97d3b00e76b707a6c30b1a877
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-10-30 01:14:54 -0500

    adding new files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from './components/Header';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
+
+//THE FOLLOWING CODE IS FOR EMAIL CODE: <a href="mailto:vick.manjarrez@gmail.com" class="btn"><button id="emailbtnhome">Email Me</button></a>
---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h1>
+          Welcome to Vick's Portfolio.  
+        </h1>
+    )
+}
+
+
+
+
+
+
+// import React, { Component } from 'react'; //always on top of every file
+// import { Container, ButtonGroup, Col, Row, Button } from 'reactstrap';
+// import { Link } from 'react-router-dom'
+// import './Home.css';
+
+// class Header extends Component {
+//     render() {
+//         return (
+
+               
+//         <div>
+//             <Container>
+//                 <header anchorX="85%" anchorY="20%">
+//                     <h1>Welcome to Vick's Portfolio</h1>
+//                 </header>
+
+//                 <Row>
+//                         <nav>
+//                         <ButtonGroup>
+//                             <Col md = {3}><Button color = "danger">Home</Button></Col>
+//                             <Col md = {3}><Button color = "danger">About Me</Button></Col>
+//                             <Col md = {3}><Button color = "danger">Projects</Button></Col>
+//                             <Col md = {3}><Button color = "danger">Contact</Button></Col>
+//                         </ButtonGroup>
+//                         </nav>
+//                 </Row>
+//             </Container>
+                   
+//         </div>        
+//         );
+//     };
+// };
+
+// export default Header;
